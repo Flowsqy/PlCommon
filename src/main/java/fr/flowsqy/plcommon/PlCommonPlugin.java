bccf2f562007f4345e1389e628d0f36b982d1469
Flowsqy <Flo504.Dev@gmail.com>
2021-11-17 14:46:35 +0100

    Init project

---
+++ src/main/java/fr/flowsqy/plcommon/PlCommonPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.plcommon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlCommonPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
